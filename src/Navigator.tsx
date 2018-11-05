efbc725e573b5ee04a90573bb6977dfb94757b0b
Sean Smith <sean.smith@tenx.tech>
2018-11-05 21:34:31 +0800

    Add prettier and .vscode for auto format on save

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,11 +1,14 @@
 import { createStackNavigator } from "react-navigation";
 
-import App from './Home';
+import App from "./Home";
 
-export default createStackNavigator({
-  home: {
-    screen: App,
-  }
-}, {
-  initialRouteName: "home",
-});
+export default createStackNavigator(
+  {
+    home: {
+      screen: App,
+    },
+  },
+  {
+    initialRouteName: "home",
+  },
+);
